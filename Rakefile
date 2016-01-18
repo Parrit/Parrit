8db0a1fea95da2d438bf6e47e58589612dec802b
Pinwheeler <anthonydreessen@gmail.com>
2016-01-17 16:15:52 -0800

    Added Jasmine command line running
    - Revamped POM test dependency structure

---
+++ Rakefile
@@ -0,0 +1,3 @@
+task :jasmine do
+  system('jasmine JASMINE_CONFIG_PATH=src/main/resources/static/js/spec/support/jasmine.json')
+end
---
+++ src/test/java/com/test/parrit/FullSuite.java
@@ -0,0 +1,11 @@
+package com.test.parrit;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+	WorkspaceControllerTests.class
+})
+public class FullSuite {
+}
