59d6326ee4a715e47d49cfc54d4eb37875316aca
Anthony Dreessen and Joe Greubel <pair+adreessen+jgreubel@pivotallabs.com>
2016-02-02 11:22:40 -0800

    connect Redux reducers to React components

--- src/main/resources/static/main.js
+++ src/main/resources/static/main.js
@@ -1,10 +1,21 @@
 // main.js
 var React = require('react');
 var ReactDOM = require('react-dom');
+var Provider = require('react-redux').Provider;
+var Redux = require('redux');
+
 var ReactClasses = require('./js/src/reactClasses.js');
+var appReducer = require('./js/src/reducers/appReducer.js');
+
+var appStore = Redux.createStore(appReducer);
 
 function run() {
-	ReactDOM.render(React.createElement(ReactClasses.App), document.getElementById('app'));
+	ReactDOM.render(
+		React.createElement(Provider, {store: appStore},
+			React.createElement(ReactClasses.App)
+		),
+		document.getElementById('app')
+	);
 }
 
 var loadedStates = ['complete', 'loaded', 'interactive'];
