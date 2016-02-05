1a9562db455aa0a17d35a9e1717e3cf76256e1a9
pivotal <pair@pivotal.io>
2016-02-05 12:16:52 -0800

    Fix requires in App.js

--- src/main/js/components/App.js
+++ src/main/js/components/App.js
@@ -1,6 +1,6 @@
 var React = require('react');
-var Menu = require('../components/menu.js');
-var Workspace = require('../components/Workspace.js');
+var Menu = require('./Menu.js');
+var Workspace = require('./Workspace.js');
 
 var App = React.createClass({
     componentDidMount: function() {
