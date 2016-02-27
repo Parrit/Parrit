4c058caf0dcb2f8d59e002d739e49a56d3b6c39f
Joe Greubel and Tommy Orr <pair+jgreubel+torr@pivotal.io>
2016-02-26 16:54:46 -0800

    Move people into first space that has less than 2 people
    [#110709736]

--- src/main/java/com/parrit/services/RecommendationService.java
+++ src/main/java/com/parrit/services/RecommendationService.java
@@ -10,6 +10,9 @@ import java.util.List;
 
 @Service
 public class RecommendationService {
+
+    private static final int FULL_SPACE_SIZE = 2;
+
     public Workspace get(Workspace workspace, List<PairingHistory> pairingHistories) {
         List<Person> floatingPeople = workspace.getPeople();
 
@@ -17,12 +20,21 @@ public class RecommendationService {
             Person floatingPerson = floatingPeople.remove(0);
 
             List<Space> spaces = workspace.getSpaces();
-            Space firstSpace = spaces.get(0);
+            Space spaceToInsert = getSpaceWithRoom(spaces);
 
-            List<Person> firstSpacePeople = firstSpace.getPeople();
+            List<Person> firstSpacePeople = spaceToInsert.getPeople();
             firstSpacePeople.add(floatingPerson);
         }
 
         return workspace;
+    }
+
+    private Space getSpaceWithRoom(List<Space> spaces) {
+        for(Space space : spaces) {
+            if(space.getPeople().size() < FULL_SPACE_SIZE) {
+                return space;
+            }
+        }
+        return spaces.get(0);
     }
 }
--- src/test/java/com/parrit/services/RecommendationServiceTest.java
+++ src/test/java/com/parrit/services/RecommendationServiceTest.java
@@ -8,6 +8,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -40,8 +41,31 @@ public class RecommendationServiceTest extends MockitoTestBase{
         space1.setId(1L);
         space1.setPeople(new ArrayList<>());
 
+        space2 = new Space();
+        space2.setId(2L);
+        space2.setPeople(new ArrayList<>());
+
+        space3 = new Space();
+        space3.setId(3L);
+        space3.setPeople(new ArrayList<>());
+
         p1 = new Person();
         p1.setId(1L);
+
+        p2 = new Person();
+        p2.setId(2L);
+
+        p3 = new Person();
+        p3.setId(3L);
+
+        p4 = new Person();
+        p4.setId(4L);
+
+        p5 = new Person();
+        p5.setId(5L);
+
+        p6 = new Person();
+        p6.setId(6L);
     }
 
     @Test
@@ -52,7 +76,7 @@ public class RecommendationServiceTest extends MockitoTestBase{
     }
 
     @Test
-    public void get_movesAFloatingPersonIntoASpace() {
+    public void get_movesAFloatingPersonIntoASpace_ifThereIsOnlyOneSpaceAndNoOneInTheSpace() {
         workspace.getPeople().add(p1);
         workspace.getSpaces().add(space1);
 
@@ -65,6 +89,45 @@ public class RecommendationServiceTest extends MockitoTestBase{
         Workspace expectedWorkspace = new Workspace();
         expectedWorkspace.setSpaces(Collections.singletonList(expectedSpace));
         expectedWorkspace.setPeople(new ArrayList<>());
+
+        assertThat(returnedWorkspace, equalTo(expectedWorkspace));
+    }
+
+    @Test
+    public void get_movesAFloatingPersonIntoAnEmptySpace_ifAllOtherSpacesHaveAtLeastTwoPeopleInThem() {
+        workspace.getPeople().add(p1);
+
+        space1.getPeople().add(p2);
+        space1.getPeople().add(p3);
+        workspace.getSpaces().add(space1);
+
+        space2.getPeople().add(p4);
+        space2.getPeople().add(p5);
+        space2.getPeople().add(p6);
+        workspace.getSpaces().add(space2);
+
+        workspace.getSpaces().add(space3);
+
+        Workspace returnedWorkspace = recommendationService.get(workspace, new ArrayList<>());
+
+        Workspace expectedWorkspace = new Workspace();
+        expectedWorkspace.setPeople(new ArrayList<>());
+        expectedWorkspace.setSpaces(new ArrayList<>());
+
+        Space space1Expected = new Space();
+        space1Expected.setId(1L);
+        space1Expected.setPeople(Arrays.asList(p2, p3));
+        expectedWorkspace.getSpaces().add(space1Expected);
+
+        Space space2Expected = new Space();
+        space2Expected.setId(2L);
+        space2Expected.setPeople(Arrays.asList(p4, p5, p6));
+        expectedWorkspace.getSpaces().add(space2Expected);
+
+        Space space3Expected = new Space();
+        space3Expected.setId(3L);
+        space3Expected.setPeople(Collections.singletonList(p1));
+        expectedWorkspace.getSpaces().add(space3Expected);
 
         assertThat(returnedWorkspace, equalTo(expectedWorkspace));
     }
