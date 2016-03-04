a8b244f64c807d97fb4f91263ff73d01d1e312ea
Joe Greubel <jgreubel@pivotal.io>
2016-03-04 12:18:29 -0800

    Weeeeeeeeeeeeeeee

--- src/main/java/com/parrit/configurations/WebSecurityConfiguration.java
+++ src/main/java/com/parrit/configurations/WebSecurityConfiguration.java
@@ -3,7 +3,9 @@ package com.parrit.configurations;
 import com.parrit.services.WorkspaceDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.access.AccessDecisionManager;
 import org.springframework.security.authentication.encoding.ShaPasswordEncoder;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -27,6 +29,7 @@ import java.io.IOException;
 
 @Configuration
 @EnableWebSecurity
+@Profile("default")
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Autowired
