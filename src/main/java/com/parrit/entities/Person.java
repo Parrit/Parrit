597b3240a0f105de2eda155e80182491c0e7d609
Dirk P. Janssen and Joe Greubel <pair+djanssen+jgreubel@pivotal.io>
2016-03-10 14:03:28 -0800

    Add length limit for Person name, BUT still produces visual box on client
    [#115382417]

--- src/main/java/com/parrit/entities/Person.java
+++ src/main/java/com/parrit/entities/Person.java
@@ -1,5 +1,7 @@
 package com.parrit.entities;
 
+import org.hibernate.validator.constraints.Length;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -14,6 +16,7 @@ public class Person {
     private long id;
 
     @NotNull
+    @Length(min = 1, max = 10)
     private String name;
 
     public Person() {
