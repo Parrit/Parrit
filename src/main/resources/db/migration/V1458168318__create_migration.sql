79cae9193c7b27249700e137eacb6eb99415dac0
Cat Zhang and Joe Greubel <pair+czhang+jgreubel@pivotal.io>
2016-03-21 14:09:53 -0700

    Update mifgration to use correct foreign key names

--- src/main/resources/db/migration/V1458168318__create_migration.sql
+++ src/main/resources/db/migration/V1458168318__create_migration.sql
@@ -1,6 +1,13 @@
 ALTER TABLE workspace RENAME TO project;
+
+ALTER TABLE space RENAME CONSTRAINT fk_workspace_id TO fk_project_id;
 ALTER TABLE space RENAME COLUMN workspace_id TO project_id;
 ALTER TABLE space RENAME TO pairing_board;
+
+ALTER TABLE person RENAME CONSTRAINT fk_workspace_id TO fk_project_id;
 ALTER TABLE person RENAME COLUMN workspace_id TO project_id;
+ALTER TABLE person RENAME CONSTRAINT fk_space_id TO fk_pairing_board_id;
 ALTER TABLE person RENAME COLUMN space_id TO pairing_board_id;
+
+ALTER TABLE pairing_history RENAME CONSTRAINT fk_workspace_id TO fk_project_id;
 ALTER TABLE pairing_history RENAME COLUMN workspace_id TO project_id;
