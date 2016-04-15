307b10621e4eee7ff61fb6a90bc3c87e5b2ff1c7
Darcie Fitzpatrick and Leonhardt Koepsell <pair+dfitzpatrick+leonhardt@pivotal.io>
2016-04-15 14:18:55 -0700

    Added feedback link to board page
    [#116337679]

--- src/main/js/project/components/Header.js
+++ src/main/js/project/components/Header.js
@@ -13,6 +13,9 @@ var Header = React.createClass({
             <a href="/" className="header-logo"/>
             <div className="links">
                 <h3 className="logout" onClick={this.props.postLogout}>LOGOUT</h3>
+                <a href="https://docs.google.com/forms/d/1Xg5xKGv0uLcRLA3WR9CiwE3tfz6-5ocGcImq6_MgLB4/viewform" target="_blank">
+                    <h3 className="feedback">feedback</h3>
+                </a>
                 <h3 className={classes} onClick={this.props.isPairingHistoryPanelOpen ? this.closePairingHistoryPanel : this.openPairingHistoryPanel}>HISTORY
                     <div className={this.props.isPairingHistoryPanelOpen ? 'history-caret-right' : 'history-caret-left'}></div>
                 </h3>
