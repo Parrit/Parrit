4dbab4fc3bd4def3058538eb23fff51ca45bc9be
Damien Le Berrigaud <damien@itsdamo.com>
2016-05-25 12:01:57 -0600

    Replace grunt-contrib-sass with grunt-sass
     - The goal is to remove the requirement for ruby in order to build/run the app.

--- gruntfile.js
+++ gruntfile.js
@@ -1,6 +1,6 @@
 module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-browserify');
-    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-sass');
 
     grunt.initConfig({
         browserify: {
