d4ea9e4d911b491ef8652cf0da1f507ce304595d
Anthony Dreessen <anthonydreessen@gmail.com>
2016-08-02 17:25:49 -0700

    Update DefaultConfiguration.java
    Change Default configuration to be the default

--- src/main/java/com/parrit/configurations/DefaultConfiguration.java
+++ src/main/java/com/parrit/configurations/DefaultConfiguration.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 @Configuration
-@Profile("cloud")
+@Profile("!cloud")
 public class DefaultConfiguration {
 
     @Bean
