f37c4eab19e6ae3662242b067d704186bb9ecd7c
Anthony Dreessen <anthonydreessen@gmail.com>
2016-09-08 19:47:25 -0700

    Add null guard case for isExempt in java

--- src/main/js/project/components/Workspace.js
+++ src/main/js/project/components/Workspace.js
@@ -54,12 +54,13 @@ var Workspace = React.createClass({
                 <h2 className="pairing-boards-title">Pairing Boards</h2>
                 <div className="pairing-boards">
                     {this.props.pairingBoards.map(function (pairingBoard, idx) {
+                        var exempt = pairingBoard.exempt ? true : false;
                         return <PairingBoard
                                             key={idx}
                                             name={pairingBoard.name}
                                             people={pairingBoard.people}
                                             index={idx}
-                                            exempt={pairingBoard.exempt}
+                                            exempt={exempt}
                                             deletePairingBoard={deletePairingBoard}
                                             renamePairingBoard={renamePairingBoard}/>;
                     })}
