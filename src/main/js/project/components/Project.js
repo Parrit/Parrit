01a5fa61a684cdb312b4ace095e90e73d939e9aa
Anthony Dreessen <anthonydreessen@gmail.com>
2017-02-22 13:52:16 -0800

    Add in tooltips to buttons

--- src/main/js/project/components/Project.js
+++ src/main/js/project/components/Project.js
@@ -39,9 +39,9 @@ var Project = React.createClass({
             <div className="sub-header">
                 <h1 className="project-name">{this.props.data.project.name}</h1>
                 <div className="project-actions">
-                    <Button className="button-blue" name="Reset Pairs" shortName="Reset" clickFunction={this.props.resetPairs} />
-                    <Button className="button-blue" name="Recommend Pairs" shortName="Recommend" clickFunction={this.props.getRecommendedPairs}/>
-                    <Button className="button-green" name="Record Pairs" shortName="Record" clickFunction={this.props.savePairing}/>
+                    <Button className="button-blue" tooltip="Move All Pairs to Floating" name="Reset Pairs" shortName="Reset" clickFunction={this.props.resetPairs} />
+                    <Button className="button-blue" tooltip="Automatically suggest pairings based on last paired date" name="Recommend Pairs" shortName="Recommend" clickFunction={this.props.getRecommendedPairs}/>
+                    <Button className="button-green" tooltip="Make note of parings for future recommendations" name="Record Pairs" shortName="Record" clickFunction={this.props.savePairing}/>
                 </div>
             </div>
             <div className="sub-header-dotted-line"></div>
