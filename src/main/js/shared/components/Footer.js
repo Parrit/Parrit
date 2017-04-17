ea359541fb063760ceeeb39cee46d531a709acf6
Joe Greubel <joegreubel1@gmail.com>
2017-04-16 21:02:28 -0700

    Add contact us link in footer
    - also bump fake copyright to 2017
    
    [#116337679]

--- src/main/js/shared/components/Footer.js
+++ src/main/js/shared/components/Footer.js
@@ -3,8 +3,11 @@ const React = require('react');
 class Footer extends React.Component {
     render() {
         return <footer>
-            <div className="fake-copyright">&copy; Parrit 2016</div>
-            <div className="github-link"><a target="_blank" href="http://www.github.com/Parrit/Parrit">GitHub</a></div>
+            <div className="fake-copyright">&copy; Parrit 2017</div>
+            <div className="footer-links">
+                <a href="mailto:parrithelp@gmail.com">Contact Us</a>
+                <a target="_blank" href="http://www.github.com/Parrit/Parrit">GitHub</a>
+            </div>
         </footer>;
     }
 }
