da01558cb4fea703b5148c3e3291d2427339b012
Joe Greubel <joegreubel1@gmail.com>
2017-12-17 15:11:42 -0800

    Remove un-needed polyfill in TestSetup

--- frontend/test/TestSetup.js
+++ frontend/test/TestSetup.js
@@ -2,13 +2,3 @@ import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 Enzyme.configure({ adapter: new Adapter() });
-
-//TODO: Tests pass locally without this. Is it needed for CI?
-window.getJasmineRequireObj = function getJasmineRequireObj() {
-  if (typeof module !== 'undefined' && module.exports) {
-    return exports;
-  } else {
-    window.jasmineRequire = window.jasmineRequire || {};
-    return window.jasmineRequire;
-  }
-}
