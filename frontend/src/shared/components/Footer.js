73075c609101ba9a515e4b1ddcf54f32fbfb1e97
Joe Greubel <joegreubel1@gmail.com>
2018-01-15 09:26:48 -0800

    Update fake copyright year

--- frontend/src/shared/components/Footer.js
+++ frontend/src/shared/components/Footer.js
@@ -4,7 +4,7 @@ export default class Footer extends React.Component {
     render() {
         return (
             <footer>
-                <div className="fake-copyright">&copy; Parrit 2017</div>
+                <div className="fake-copyright">&copy; Parrit 2018</div>
                 <div className="footer-links">
                     <a target="_blank" rel="noopener" href="mailto:parrithelp@gmail.com">Contact Us</a>
                     <a target="_blank" rel="noopener" href="http://www.github.com/Parrit/Parrit">GitHub</a>
