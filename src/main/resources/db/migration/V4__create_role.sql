3964858fdf1a2373ac325e59b2e0993d10f15cfb
Joe Greubel <joegreubel1@gmail.com>
2018-01-31 21:09:54 -0800

    Add stricter database rules for role table
    Make pairing board id NOT NULL, add foreign key for that column, and link role id sequence with role id column.

--- src/main/resources/db/migration/V4__create_role.sql
+++ src/main/resources/db/migration/V4__create_role.sql
@@ -9,10 +9,13 @@ CREATE SEQUENCE role_id_seq
 CREATE TABLE role (
     id bigint DEFAULT nextval('role_id_seq'::regclass) NOT NULL,
     name character varying(255) NOT NULL,
-    pairing_board_id bigint
+    pairing_board_id bigint NOT NULL
 );
 
 ALTER TABLE ONLY role
     ADD CONSTRAINT role_pkey PRIMARY KEY (id);
 
+ALTER TABLE ONLY role
+    ADD CONSTRAINT pairing_board_id_fk FOREIGN KEY (pairing_board_id) REFERENCES pairing_board(id);
 
+ALTER SEQUENCE role_id_seq OWNED BY role.id;
