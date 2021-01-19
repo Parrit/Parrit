c2625f745cfaadd2dcee53936d966c11d30a8351
Anthony Dreessen <anthonydreessen@gmail.com>
2021-01-19 13:57:01 -0800

    delete reducers

--- frontend/src/project/reducers/AppReducer.ts
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from "redux";
-
-import dataReducer from "./DataReducer.js";
-import settingsReducer from "./SettingsReducer.js";
-
-export default combineReducers({
-  data: dataReducer,
-  settings: settingsReducer,
-});
