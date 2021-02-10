91fe323c2398efbed29f9b93598338c462702c7d
Anthony Dreessen <anthonydreessen@gmail.com>
2021-02-09 18:00:52 -0800

    WIP repairing tests

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -3,8 +3,4 @@ const { defaults } = require("jest-config");
 module.exports = {
   moduleFileExtensions: [...defaults.moduleFileExtensions, "ts", "tsx"],
   testMatch: ["<rootDir>/src/**/*.spec.tsx"],
-  setupFiles: [
-    "<rootDir>/polyfills.js",
-    "<rootDir>/src/shared/misc/TestSetup.js",
-  ],
 };
--- frontend/src/dashboard/components/Dashboard.spec.tsx
+++ frontend/src/dashboard/components/Dashboard.spec.tsx
@@ -1,130 +1,49 @@
 import React from "react";
-import { mount, ReactWrapper } from "enzyme";
 
 import { Dashboard } from "./Dashboard";
 import { ApiContext, IApiContext } from "../../shared/helpers/ApiContext";
 import { DefaultContexts } from "../../shared/helpers/DefaultContexts";
+import TestRenderer, { ReactTestInstance } from "react-test-renderer";
 
 describe("<Dashboard/>", () => {
-  let wrapper: ReactWrapper;
+  let root: ReactTestInstance;
   let apiContext: IApiContext;
+  let newProjectNameInput: ReactTestInstance;
+  let newProjectPasswordInput: ReactTestInstance;
+  let loginProjectName: ReactTestInstance;
+  let loginProjectPassword: ReactTestInstance;
 
   beforeEach(() => {
     apiContext = DefaultContexts.apiContext();
-    wrapper = mount(
+    root = TestRenderer.create(
       <ApiContext.Provider value={apiContext}>
         <Dashboard />
       </ApiContext.Provider>
-    );
+    ).root;
+    newProjectNameInput = root.findAllByProps({
+      placeholder: "Project name",
+    })[0];
+    newProjectPasswordInput = root.findAllByProps({
+      placeholder: "Password",
+    })[0];
+    loginProjectName = root.findAllByProps({ placeholder: "Project name" })[1];
+    loginProjectPassword = root.findAllByProps({ placeholder: "Password" })[1];
   });
 
   it("does not show an error by default", () => {
-    expect(wrapper.find(".new-form .error-message").at(0).text()).toBe("");
+    expect(
+      root
+        .findByProps({ className: "form new-form" })
+        .findByProps({ className: "error-message" }).children
+    ).toBe([]);
   });
 
-  describe("posting a new project", () => {
-    describe("when it results in an error", () => {
-      beforeEach(() => {
-        apiContext.postProject = jest
-          .fn()
-          .mockRejectedValue(new Error("test error"));
-      });
-    });
-    it("displays a project name error message when the newProjectErrorMessage is present", () => {
-      wrapper = shallow(<InnerDashboard {...props} />);
-
-      expect(wrapper.find(".new-form .error-message").text()).toBe(
-        "some new project error message"
-      );
+  describe("with a filled out new project form", () => {
+    beforeEach(() => {
+      newProjectNameInput.props.onChange("NEW PROJECT NAME");
+      newProjectPasswordInput.props.onChange("NEW PROJECT PASSWORD");
     });
 
-    it("highlights the name field when the newProjectErrorName is true", () => {
-      props.newProjectErrorName = true;
-
-      wrapper = shallow(<InnerDashboard {...props} />);
-
-      expect(wrapper.find(".new-form input").at(0).prop("className")).toContain(
-        "error"
-      );
-    });
-
-    it("highlights the password field when the newProjectErrorPassword is true", () => {
-      props.newProjectErrorPassword = true;
-
-      wrapper = shallow(<InnerDashboard {...props} />);
-
-      expect(wrapper.find(".new-form input").at(1).prop("className")).toContain(
-        "error"
-      );
-    });
-  });
-
-  describe("#loginError", () => {
-    it("does not display a login error message when loginErrorMessage is undefined", () => {
-      expect(wrapper.find(".login-form .error-message").text()).toBe("");
-    });
-
-    it("displays a login error message when the loginErrorMessage is present", () => {
-      props.loginErrorMessage = "some login error message";
-
-      wrapper = shallow(<InnerDashboard {...props} />);
-
-      expect(wrapper.find(".login-form .error-message").text()).toBe(
-        "some login error message"
-      );
-    });
-
-    it("highlights the name field when the loginErrorName is true", () => {
-      props.loginErrorName = true;
-
-      wrapper = shallow(<InnerDashboard {...props} />);
-
-      expect(
-        wrapper.find(".login-form input").at(0).prop("className")
-      ).toContain("error");
-    });
-
-    it("highlights the name field when the loginErrorPassword is true", () => {
-      props.loginErrorPassword = true;
-
-      wrapper = shallow(<InnerDashboard {...props} />);
-
-      expect(
-        wrapper.find(".login-form input").at(1).prop("className")
-      ).toContain("error");
-    });
-  });
-
-  describe("#createProjectWithName", () => {
-    const event = { preventDefault: jasmine.createSpy("preventDefaultSpy") };
-
-    it("calls the login function with the username and password on the state", () => {
-      wrapper.setState({ newProjectName: "Hello", newProjectPassword: "Bye" });
-      wrapper.find(".new-form").simulate("submit", event);
-      expect(props.createProject).toHaveBeenCalledWith("Hello", "Bye");
-    });
-
-    it("calls preventDefault on the event", () => {
-      wrapper.find(".new-form").simulate("submit", event);
-      expect(event.preventDefault).toHaveBeenCalled();
-    });
-  });
-
-  describe("#handleLogin", () => {
-    const event = { preventDefault: jasmine.createSpy("preventDefaultSpy") };
-
-    it("calls the login function with the username and password on the state", () => {
-      wrapper.setState({
-        loginProjectName: "Hello",
-        loginProjectPassword: "Bye",
-      });
-      wrapper.find(".login-form").simulate("submit", event);
-      expect(props.login).toHaveBeenCalledWith("Hello", "Bye");
-    });
-
-    it("calls preventDefault on the event", () => {
-      wrapper.find(".login-form").simulate("submit", event);
-      expect(event.preventDefault).toHaveBeenCalled();
-    });
+    describe("With an error", () => {});
   });
 });
