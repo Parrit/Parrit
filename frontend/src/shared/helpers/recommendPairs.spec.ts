7ec0f44f1ee96c9ec0070fc39b3806faa3d25d5a
Anthony Dreessen <anthonydreessen@gmail.com>
2021-03-24 21:39:38 -0700

    passing initial pair construction test

--- frontend/src/shared/helpers/recommendPairs.spec.ts
+++ frontend/src/shared/helpers/recommendPairs.spec.ts
@@ -14,7 +14,14 @@ describe("building the timetable", () => {
     subject = new ProjectHelper(project, history);
   });
 
-  it("constructs the timetible as expected", () => {
-    expect(subject.timetable).toEqual({});
+  it("constructs the timetable as expected", () => {
+    expect(subject.timetable).toEqual({
+      "-2&&-1": new Date("2021-01-22T03:02:33.288Z"),
+      "-3&&-1": new Date("2021-01-18T03:02:33.288Z"),
+      "-3&&-2": new Date("2021-01-17T03:02:33.288Z"),
+      "-4&&-1": new Date("2021-01-17T03:02:33.288Z"),
+      "-4&&-2": new Date("2021-01-20T03:02:33.288Z"),
+      "-4&&-3": new Date("2021-01-19T03:02:33.288Z"),
+    });
   });
 });
