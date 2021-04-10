747ddd083ce3ee994bdfc94b4d8f7d7c1fde3949
hanchen <hanchenjhc@gmail.com>
2021-04-10 16:16:00 -0500

    Use PersonRepository to retrieve all people instead of using custom query.

--- src/main/java/com/parrit/repositories/CustomizedPairingHistoryRepositoryImpl.java
+++ src/main/java/com/parrit/repositories/CustomizedPairingHistoryRepositoryImpl.java
@@ -12,49 +12,58 @@ import java.sql.SQLException;
 import java.sql.Timestamp;
 import java.util.*;
 
-import static java.util.Collections.nCopies;
-import static java.util.stream.Collectors.toList;
-import static java.util.stream.Collectors.toSet;
+import static java.util.stream.Collectors.*;
 
 @Repository
 public class CustomizedPairingHistoryRepositoryImpl implements CustomizedPairingHistoryRepository {
 
     private final JdbcTemplate jdbcTemplate;
+    private final PersonRepository personRepository;
 
     @Autowired
-    public CustomizedPairingHistoryRepositoryImpl(JdbcTemplate jdbcTemplate) {
+    public CustomizedPairingHistoryRepositoryImpl(JdbcTemplate jdbcTemplate, PersonRepository personRepository) {
         this.jdbcTemplate = jdbcTemplate;
+        this.personRepository = personRepository;
     }
 
     @Override
     public List<PairingHistory> findByProjectAndTimestampAfterOrderByTimestampDesc(Project project, Timestamp timestamp) {
-        Map<PairingHistory, Long[]> pairingHistoriesToPersonIds = getPairingHistoriesToPersonIds(project, timestamp);
-        Map<Long, Person> peopleInHistory = getRelevantPeople(pairingHistoriesToPersonIds);
+        Map<PairingHistory, Set<Long>> pairingHistoriesToPersonIds = getPairingHistoriesToPersonIds(project, timestamp);
+        Map<Long, Person> peopleInHistory = getPeople(getPersonIdsInHistory(pairingHistoriesToPersonIds));
 
         return pairingHistoriesToPersonIds.entrySet().stream()
-                .map(entry -> entry.getKey()
-                                .withPeople(Arrays.stream(entry.getValue()).map(id -> peopleInHistory.get(id)).collect(toList()))
-                )
+                .map(entry -> entry.getKey().withPeople(getPersonsForHistory(peopleInHistory, entry.getValue())))
                 .sorted((a, b) -> b.getTimestamp().compareTo(a.getTimestamp()))
                 .collect(toList());
     }
 
-    private Map<PairingHistory, Long[]> getPairingHistoriesToPersonIds(Project project, Timestamp timestamp) {
+    private List<Person> getPersonsForHistory(Map<Long, Person> peopleInHistory, Set<Long> personIds) {
+        return personIds.stream().map(peopleInHistory::get).collect(toList());
+    }
+
+    private Set<Long> getPersonIdsInHistory(Map<PairingHistory, Set<Long>> pairingHistoriesToPersonIds) {
+        return pairingHistoriesToPersonIds.values().stream().flatMap(Collection::stream).collect(toSet());
+    }
+
+    private Map<PairingHistory, Set<Long>> getPairingHistoriesToPersonIds(Project project, Timestamp timestamp) {
         return jdbcTemplate.query(
                 "SELECT id, pairing_board_name, timestamp, array_agg(person_id) as persons " +
                         "FROM pairing_history ph JOIN pairing_history_people php ON php.pairing_history_id = ph.id " +
                         "WHERE project_id = ? AND timestamp > ? " +
                         "GROUP BY id;",
-                resultSet -> { return extractPairingHistoryToPersonIds(project, resultSet); },
+                resultSet -> {
+                    return extractPairingHistoryToPersonIds(project, resultSet);
+                },
                 project.getId(),
                 timestamp
         );
     }
 
-    private Map<PairingHistory, Long[]> extractPairingHistoryToPersonIds(Project project, ResultSet resultSet) throws SQLException {
-        Map<PairingHistory, Long[]> result = new HashMap<>();
+    private Map<PairingHistory, Set<Long>> extractPairingHistoryToPersonIds(Project project, ResultSet resultSet) throws SQLException {
+        Map<PairingHistory, Set<Long>> result = new HashMap<>();
         while (resultSet.next()) {
             Long[] personIds = (Long[]) resultSet.getArray("persons").getArray();
+
             result.put(
                     PairingHistory.builder()
                             .id(resultSet.getLong("id"))
@@ -62,32 +71,14 @@ public class CustomizedPairingHistoryRepositoryImpl implements CustomizedPairing
                             .project(project)
                             .timestamp(resultSet.getTimestamp("timestamp"))
                             .build(),
-                    personIds
+                    new HashSet<>(Arrays.asList(personIds))
             );
         }
         return result;
     }
 
-    private Map<Long, Person> getRelevantPeople(Map<PairingHistory, Long[]> pairingHistories) {
-        Set<Long> personIds = pairingHistories.values().stream().flatMap(Arrays::stream).collect(toSet());
-        String inClausePlaceholder = String.join(",", nCopies(personIds.size(), "?"));
-
-        return jdbcTemplate.query(
-                String.format("SELECT id, name FROM person WHERE id IN (%s)", inClausePlaceholder),
-                this::extractIdToPerson,
-                personIds.toArray());
-    }
-
-    private Map<Long, Person> extractIdToPerson(ResultSet resultSet) throws SQLException {
-        Map<Long, Person> result = new HashMap<>();
-
-        while (resultSet.next()) {
-            long personId = resultSet.getLong("id");
-            result.put(
-                    personId,
-                    Person.builder().id(personId).name(resultSet.getString("name")).build()
-            );
-        }
-        return result;
+    private Map<Long, Person> getPeople(Set<Long> personIds) {
+        return personRepository.findAllById(personIds).stream()
+                .collect(groupingBy(Person::getId, reducing(new Person(), (a, b) -> b)));
     }
 }
