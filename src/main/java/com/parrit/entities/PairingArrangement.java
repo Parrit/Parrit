19178d794fbe8c321e6cc5ddc579460848706a6c
hanchen <hanchenjhc@gmail.com>
2021-04-30 17:58:43 -0500

    Use pairing_arrangement table to support PairingArrangement
    normalize pairing_history and pairing_arrangement data
    
    + not using the custom query at this moment to see if performance for pairing history in staging is acceptable
    
    [#177807748]

--- src/main/java/com/parrit/entities/PairingArrangement.java
+++ src/main/java/com/parrit/entities/PairingArrangement.java
@@ -12,6 +12,7 @@ import java.util.Set;
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
 @EqualsAndHashCode
 @ToString
 public class PairingArrangement {
@@ -19,14 +20,14 @@ public class PairingArrangement {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
-    @OneToMany
+    @OneToMany(cascade = CascadeType.ALL)
     @JoinColumn(name = "pairing_arrangement_id", foreignKey = @ForeignKey(name = "FK_pairing_history__pairing_arrangement"))
     Set<PairingHistory> pairingHistories;
 
     @Column(name = "pairing_time", nullable = false)
-    private final Timestamp pairingTime;
+    private Timestamp pairingTime;
 
     @ManyToOne
     @JoinColumn(name = "project_id", nullable = false, foreignKey = @ForeignKey(name = "FK_pairing__arrangement_project"))
-    private final Project project;
+    private Project project;
 }
--- src/main/resources/db/migration/V6__create_pairing_arrangement_table.sql
+++ src/main/resources/db/migration/V6__create_pairing_arrangement_table.sql
@@ -1,7 +1,18 @@
-CREATE TABLE pairing_arrangement (
-    id SERIAL PRIMARY KEY,
+CREATE TABLE pairing_arrangement
+(
+    id           BIGSERIAL PRIMARY KEY,
     pairing_time timestamp without time zone NOT NULL,
-    project_id bigint NOT NULL
-)
+    project_id   bigint                      NOT NULL
+);
 
--- add forein key column and create foreign key in pairing history
+ALTER TABLE pairing_history
+    ADD COLUMN pairing_arrangement_id bigint;
+
+ALTER TABLE pairing_history
+    ADD CONSTRAINT FK_pairing_history__pairing_arrangement
+        FOREIGN KEY (pairing_arrangement_id)
+            REFERENCES pairing_arrangement (id);
+
+ALTER TABLE pairing_history DROP CONSTRAINT IF EXISTS project_id_fk;
+ALTER TABLE pairing_history ALTER COLUMN project_id DROP NOT NULL;
+ALTER TABLE pairing_history ALTER COLUMN timestamp DROP NOT NULL;
